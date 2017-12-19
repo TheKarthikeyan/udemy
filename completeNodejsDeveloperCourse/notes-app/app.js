df3905678f3083732ccf683c76e81b0fb5cbb590
Karthikeyan M <karthikeyanms@freshdesk.com>
2017-12-19 19:04:51 +0530

    Cleaned files for notes app

--- completeNodejsDeveloperCourse/notes-app/app.js
+++ completeNodejsDeveloperCourse/notes-app/app.js
@@ -1,24 +1,6 @@
 console.log('Starting app.js')
 
 const fs = require('fs');
-const os = require('os');
-const _ = require('lodash')
-const notes = require('./notes.js')
+const _ = require('lodash');
 
-// console.log(_.isString(true));
-// console.log(_.isString("Karthik"));
-
-var filteredArray = _.uniq([1,1,2,3,3,45,6,6,"Karthik","Karthik",1,"2"]);
-
-console.log(filteredArray);
-
-// var result = notes.addNote();
-// console.log(result);
-
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
-//   if(err) {
-//     console.log("Unable to write to file");
-//   }
-// });
+const notes = require('./notes.js');
