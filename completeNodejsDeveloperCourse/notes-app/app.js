46eea3528f5c8f21282dab9028bd510bf03ca045
Karthikeyan M <karthikeyanms@freshdesk.com>
2017-12-19 18:56:53 +0530

    Learned loads, third party modules import

--- completeNodejsDeveloperCourse/notes-app/app.js
+++ completeNodejsDeveloperCourse/notes-app/app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js')
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
 const notes = require('./notes.js')
 
-console.log(notes.add(-9,-2));
+// console.log(_.isString(true));
+// console.log(_.isString("Karthik"));
+
+var filteredArray = _.uniq([1,1,2,3,3,45,6,6,"Karthik","Karthik",1,"2"]);
+
+console.log(filteredArray);
 
 // var result = notes.addNote();
 // console.log(result);
