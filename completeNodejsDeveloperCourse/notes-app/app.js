2dd282a80be6fc5b69276946268673110711b36e
Karthikeyan M <karthikeyanms@freshdesk.com>
2017-12-24 11:46:24 +0530

    Remove note added

--- completeNodejsDeveloperCourse/notes-app/app.js
+++ completeNodejsDeveloperCourse/notes-app/app.js
@@ -9,17 +9,26 @@ const notes = require('./notes.js');
 const argv = yargs.argv;
 var command = argv._[0]
 console.log('Command: ',command);
-console.log('Process ',process.argv);
 console.log('Yargs ',argv);
 
 if(command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  addStatus = notes.addNote(argv.title, argv.body);
+  if(_.eq(undefined,addStatus)){
+    console.log("Duplicate note entered")
+  } else {
+    console.log("Successfully saved note ");
+    console.log("---");
+    console.log(`Title: ${addStatus.title}`);
+    console.log(`Body: ${addStatus.body}`);
+  }
 } else if(command === 'list') {
   notes.getAll();
 } else if(command === 'read') {
   notes.getNote(argv.title);
 } else if(command === 'remove') {
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note Successfully removed': 'Note not found';
+  console.log(message);
 } else {
   console.log('Command not recognised');
 }
