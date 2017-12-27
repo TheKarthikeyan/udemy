bc418019f3843b09d22f76575695a86d6e39299a
Karthikeyan M <karthikeyanms@freshdesk.com>
2017-12-27 11:04:25 +0530

    Geocode done & Weather boilerplate

--- completeNodejsDeveloperCourse/weather-app/app.js
+++ completeNodejsDeveloperCourse/weather-app/app.js
@@ -1,8 +1,40 @@
+// const yargs = require('yargs')
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help','h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if(errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+// forecast-api key: ffe4f0930d48d353e84d53bbf52ffa99
+
 const request = require('request');
 
 request({
-  url:'https://maps.googleapis.com/maps/api/geocode/json?address=18th%20street%20tansi%20nagar%20velachery',
+  url: `https://api.darksky.net/forecast/ffe4f0930d48d353e84d53bbf52ffa99/11.0948328,77.04493269999999`,
   json: true
-}, (error, response, body)=> {
-  console.log(JSON.stringify(body, undefined, 2));
+},(error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch weather');
+  }
+
 });
