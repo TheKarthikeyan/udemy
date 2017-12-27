f15384da4ba0af641d06ffd958862a6fe2a5310c
Karthikeyan M <karthikeyanms@freshdesk.com>
2017-12-27 23:56:32 +0530

    Chapter 4 done

--- completeNodejsDeveloperCourse/weather-app/app.js
+++ completeNodejsDeveloperCourse/weather-app/app.js
@@ -1,40 +1,32 @@
-// const yargs = require('yargs')
-//
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help','h')
-//   .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if(errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+const yargs = require('yargs')
 
-// forecast-api key: ffe4f0930d48d353e84d53bbf52ffa99
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-const request = require('request');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help','h')
+  .argv;
 
-request({
-  url: `https://api.darksky.net/forecast/ffe4f0930d48d353e84d53bbf52ffa99/11.0948328,77.04493269999999`,
-  json: true
-},(error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    console.log(body.currently.temperature);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if(errorMessage) {
+    console.log(errorMessage);
   } else {
-    console.log('Unable to fetch weather');
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude,(errorMessage, weatherResults) => {
+      if(errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+      }
+    });
   }
-
 });
